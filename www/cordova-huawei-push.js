d1c5334da9a5c5404511daac87a4dec0c9ad37e6
waitaction <350561378@qq.com>
2017-04-29 18:22:03 +0800

    优化

--- www/cordova-huawei-push.js
+++ www/cordova-huawei-push.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 
-var HuaweiPush = function () {}
-HuaweiPush.prototype.isAndroidDevice = function(){
+var HuaweiPush = function () { }
+HuaweiPush.prototype.isAndroidDevice = function () {
     return device.platform == 'Android';
 }
 // 获取到token
@@ -9,7 +9,7 @@ HuaweiPush.prototype.tokenRegistered = function (token) {
     try {
         this.receiveRegisterResult = token;
         cordova.fireDocumentEvent('huaweipush.receiveRegisterResult', this.receiveRegisterResult);
-    } catch(exception) {
+    } catch (exception) {
         console.log('HuaweiPush:tokenRegistered ' + exception);
     }
 }
@@ -19,10 +19,11 @@ HuaweiPush.prototype.pushMsgReceived = function (msg) {
         msg.extras = JSON.parse(msg.extras);
         this.receiveRegisterResult = msg;
         cordova.fireDocumentEvent('huaweipush.pushMsgReceived', this.receiveRegisterResult);
-    } catch(exception) {
+    } catch (exception) {
         console.log('HuaweiPush:pushMsgReceived ' + exception);
     }
 }
+//通知消息
 HuaweiPush.prototype.notificationOpened = function (msg) {
     try {
         console.log(msg);
@@ -30,19 +31,58 @@ HuaweiPush.prototype.notificationOpened = function (msg) {
         msg.extras = JSON.parse(msg.extras);
         this.receiveRegisterResult = msg;
         cordova.fireDocumentEvent('huaweipush.notificationOpened', this.receiveRegisterResult);
-    } catch(exception) {
+    } catch (exception) {
         console.log('HuaweiPush:notificationOpened ' + exception);
     }
 }
-HuaweiPush.prototype.init = function(success, error) {
+//初始
+HuaweiPush.prototype.init = function (success, error) {
     if (this.isAndroidDevice()) {
         exec(success, error, "CordovaHuaweiPush", "init", []);
     }
 };
-HuaweiPush.prototype.stop = function(success, error) {
+//停止hms服务
+HuaweiPush.prototype.stop = function (success, error) {
     if (this.isAndroidDevice()) {
         exec(success, error, "CordovaHuaweiPush", "stop", []);
     }
 };
 
+//连接到hms服务
+HuaweiPush.prototype.connect = function () {
+    if (this.isAndroidDevice()) {
+        exec(success, error, "CordovaHuaweiPush", "connect", []);
+    }
+}
+
+//是否已连接
+HuaweiPush.prototype.isConnected = function () {
+    if (this.isAndroidDevice()) {
+        exec(success, error, "CordovaHuaweiPush", "isConnected", []);
+
+    }
+}
+
+//付款 productName:商品名 productDesc:商品描述 amount:金额 requestId:订单号 merchantName:商户名称 extReserved:商户保留信息,回调给商户服务端
+HuaweiPush.prototype.pay = function (payReq) {
+    if (this.isAndroidDevice()) {
+        exec(success, error, "CordovaHuaweiPush", "pay", [payReq]);
+    }
+}
+//输出调试日志
+HuaweiPush.prototype.log = function (logStr) {
+    if (this.isAndroidDevice()) {
+        try {
+            cordova.fireDocumentEvent('huaweipush.log', logStr);
+        } catch (exception) {
+            console.log('HuaweiPush:tokenRegistered ' + exception);
+        }
+    }
+}
+//配置 userId  appId rsaKeyPrivate rsaKeyPublic
+HuaweiPush.prototype.config = function (configInfo) {
+    if (this.isAndroidDevice()) {
+        exec(success, error, "CordovaHuaweiPush", "config", [configInfo]);
+    }
+}
 module.exports = new HuaweiPush();
